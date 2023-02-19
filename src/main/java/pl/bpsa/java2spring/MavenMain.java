418303bcbaa2530ace5cf9e4b3c2d4d37966c543
aras112 <aras112nowe@o2.pl>
2023-02-19 12:33:42 +0100

    init

--- src/main/java/pl/bpsa/java2spring/MavenMain.java
+++ src/main/java/pl/bpsa/java2spring/MavenMain.java
@@ -4,30 +4,26 @@ import lombok.Getter;
 import lombok.Setter;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
 import pl.bpsa.java2spring.model.ApiModel;
 
 @Mojo(name = "cxf2spring", defaultPhase = LifecyclePhase.PROCESS_RESOURCES)
 public class MavenMain extends AbstractMojo {
 
-    @Parameter(property = "apiClass")
-    @Getter
-    @Setter
-    private String clazz;
-
-    @Parameter(property = "packageName")
-    @Getter
-    @Setter
-    private String packageName;
-
     @Parameter(defaultValue = "path", required = true, readonly = true)
     @Getter
     @Setter
     String path;
+    @Parameter(property = "apiClass")
+    @Getter
+    @Setter
+    private String clazz;
+    @Parameter(property = "packageName")
+    @Getter
+    @Setter
+    private String packageName;
 
     @Override
     public void execute() throws MojoExecutionException {
@@ -39,7 +35,7 @@ public class MavenMain extends AbstractMojo {
             ClassSaver.save(print, path, api);
 
         } catch (Exception e) {
-            throw new MojoExecutionException(e);
+            throw new MojoExecutionException(e.getMessage());
         }
     }
 }
